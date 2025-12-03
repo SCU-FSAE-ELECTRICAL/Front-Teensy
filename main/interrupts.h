94217f1aaaecccd75c38d83fb6c04333c564481a
harinimn <121125480+harinimn@users.noreply.github.com>
2025-12-03 12:11:50 -0800

    Interrupts added-2

--- main/interrupts.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void initTorqueInterrupt();
-void initFaultCheckInterrupt();
-void initTempCheckInterrupt();
-void torqueISR();
-void faultCheckISR();
-void tempCheckISR();
-void resetFaults();  
